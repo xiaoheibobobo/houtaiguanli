62a362d9120a5469f033ce26af45d21da00e2cc7
xiaohei <xiaoheibobobo@163.com>
2018-10-31 20:25:45 +0800

    登录页面渲染

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+import Login from '@/views/login'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-  ]
+  routes: [{
+    path: '/login',
+    component: Login
+  }]
 })
